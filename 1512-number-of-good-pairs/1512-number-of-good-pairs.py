22a9e21a126a6149a8359e21079e9bff8db23d87
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-08-02 00:40:41 -0700

    Time: 39 ms (40.97%), Space: 16.6 MB (19.79%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,9 +1,30 @@
+from collections import Counter
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        """
+        n^2 complexity
         gp=0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]==nums[j]:
                     gp+=1
         return gp
+        """
+        """
+        Hashmap approach
+        count=Counter(nums)
+        result=0
+        for v in count.values():
+            result+=(v*(v-1))//2
+        return result
+        """
+        count={}
+        result=0
+        for n in nums:
+            if n in count:
+                result+=count[n]
+                count[n]+=1
+            else:
+                count[n]=1
+        return result
         
