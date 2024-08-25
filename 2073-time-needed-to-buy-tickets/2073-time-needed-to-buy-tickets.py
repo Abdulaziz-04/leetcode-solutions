27bb8a0bbda98c1437d74d65d73ba290ad3cd6ca
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-08-25 10:36:58 -0700

    Time: 38 ms (78.18%), Space: 16.4 MB (95.65%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -2,9 +2,13 @@ class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         time=0
         for i in range(len(tickets)):
+            # We only care for the kth customer, so we collect those many tickets on the run
             if i<=k:
                 time+=min(tickets[i],tickets[k])
             else:
-                time += min(tickets[i],tickets[k]-1)
+                # After our customer, the last round won't continue till the end
+                # i.e. second last round only
+                time+=min(tickets[i],tickets[k]-1)
         return time
+
         
