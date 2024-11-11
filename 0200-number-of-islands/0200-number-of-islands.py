4bfcb508e28343fde05abdfc1a966d972281e6d1
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-11-11 16:37:29 -0700

    Time: 263 ms (31.65%), Space: 24.5 MB (17.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,32 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # BFS
+        def bfs(r,c):
+            q=deque()
+            q.append((r,c))
+            visited.add((r,c))
+            while q:
+                r,c=q.popleft()
+                directions=[[0,1],[1,0],[0,-1],[-1,0]]
+                for dr,dc in directions:
+                    tr,tc=r+dr,c+dc
+                    if (tr>=0 and tr<R) and (tc>=0 and tc<C) and grid[tr][tc]=="1" and (tr,tc) not in visited:
+                        q.append((tr,tc))
+                        visited.add((tr,tc))
+        if not grid:
+            return 0
+        R,C=len(grid),len(grid[0])
+        visited=set()
+        islands=0
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c]=="1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands+=1
+        return islands
+        """
+        # DFS
         if not grid:
             return 0
         R,C=len(grid),len(grid[0])
@@ -19,6 +46,7 @@ class Solution:
                     islands+=1
                     dfs(r,c)
         return islands
+        """
 
 
         
