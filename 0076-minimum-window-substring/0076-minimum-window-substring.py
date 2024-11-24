3112bf765cd42a71294f38a646a59deb1fed4314
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-11-24 02:00:33 -0700

    Time: 67 ms (77.7%), Space: 16.9 MB (90.67%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,29 @@
-from collections import defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        # Base Case
         if t=="": return ""
 
-        t_count,window=defaultdict(int),defaultdict(int)
+        # Variables
+        window,t_count=defaultdict(int),defaultdict(int)
+        result,mxLen=[-1,-1],float('inf')
+        l=0
+
         for ch in t:
             t_count[ch]+=1
         have,need=0,len(t_count)
-        l=0
-        res,mx_len=[-1,-1],float('inf')
         for r in range(len(s)):
-            ch=s[r]
-            window[ch]+=1
-            if ch in t_count and t_count[ch]==window[ch]:
+            window[s[r]]+=1
+            if s[r] in t_count and window[s[r]]==t_count[s[r]]:
                 have+=1
-            while have ==need:
-                if r-l+1 < mx_len:
-                    mx_len=r-l+1
-                    res=[l,r]
+            while have==need:
+                if r-l+1<mxLen:
+                    mxLen=r-l+1
+                    result=[l,r]
                 window[s[l]]-=1
-                if s[l] in t_count and t_count[s[l]]>window[s[l]]:
+                if s[l] in t_count and window[s[l]]<t_count[s[l]]:
                     have-=1
                 l+=1
-        l,r=res
-        return s[l:r+1] if mx_len!=float('inf') else ""
-            
-
+        l,r=result
+        return s[l:r+1] if mxLen!=float('inf') else ""
 
         
