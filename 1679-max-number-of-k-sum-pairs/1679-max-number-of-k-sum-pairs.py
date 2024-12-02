54a03a479a34236f095dbf105646b79db352e932
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-12-01 17:00:26 -0700

    Time: 524 ms (13.64%), Space: 29.2 MB (48.09%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        if len(nums)<2:
-            return 0
-        freq_map={}
-        for n in nums:
-            if n not in freq_map:
-                freq_map[n]=1
+        nums.sort()
+        l,r=0,len(nums)-1
+        operations=0
+        while l<r:
+            total=nums[l]+nums[r]
+            if total<k:
+                l+=1
+            elif total>k:
+                r-=1
             else:
-                freq_map[n]+=1
-        pairs=0
-        for n in nums:
-            # Check for opp. number in map and check if current number has sufficient freq
-            if k-n in freq_map and freq_map[n]>0:
-                freq_map[n]-=1
-                if freq_map[k-n]>0:
-                    freq_map[k-n]-=1
-                    pairs+=1
-        return pairs
+                l+=1
+                r-=1
+                operations+=1
+        return operations
 
         
