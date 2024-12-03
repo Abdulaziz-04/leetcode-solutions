e621294c072ad237cb09bb3d0baf354eea24aae2
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-12-02 23:09:06 -0700

    Time: 189 ms (5%), Space: 18 MB (6.5%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,28 +1,18 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        """
-        Use variable length sliding window here,
-        keep track of current window length and if it violates the <=k zeros 
-        we increment the left pointer else the right pointer
-        """
-        # Define initial variables
+        mxLen=0
         l=0
-        l_consec=0
         zero_count=0
-
         for r in range(len(nums)):
-            # keep track of zero count and maintain valid window
             if nums[r]==0:
                 zero_count+=1
-            # If invalid window, make it valid again
             while zero_count>k:
                 if nums[l]==0:
                     zero_count-=1
-                l+=1    
-            # Check the window length
-            l_consec=max(l_consec,r-l+1)
-        return l_consec
-                
+                l+=1
+            mxLen=max(mxLen,r-l+1)
+            print(zero_count)
+        return mxLen
 
-       
+
         
