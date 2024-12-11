f6709ae6fe7f4901f4dc4c4c728df88501250155
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2024-12-11 01:21:15 -0700

    Time: 3 ms (90.28%), Space: 12.1 MB (14.68%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,35 @@
-from collections import deque
-class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        R,C=len(grid),len(grid[0])
-        time=0
-        fresh=0
+class Solution(object):
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         q=deque()
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c]==1:
+        visited=set()
+        fresh=0
+        r=len(grid)
+        c=len(grid[0])
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j]==1:
                     fresh+=1
-                elif grid[r][c]==2:
-                    q.append((r,c))
-        if fresh==0:
-            return 0
-        directions=[[0,1],[1,0],[-1,0],[0,-1]]
+                elif grid[i][j]==2:
+                    q.append((i,j))
+                    visited.add((i,j))
+        
+        time=0
+        directions=[[1,0],[0,1],[-1,0],[0,-1]]
         while q and fresh>0:
             for i in range(len(q)):
-                r,c=q.popleft()
-                for dr,dc in directions:
-                    nr,nc=r+dr,c+dc
-                    if nr>=0 and nr<R and nc>=0 and nc<C and grid[nr][nc]==1:
-                        grid[nr][nc]=2
-                        q.append((nr,nc))
+                cx,cy=q.popleft()
+                for dx,dy in directions:
+                    nx,ny=cx+dx,cy+dy
+                    if nx>=0 and nx<r and ny>=0 and ny<c and grid[nx][ny]==1 and (nx,ny) not in visited:
+                        q.append((nx,ny))
+                        visited.add((nx,ny))
                         fresh-=1
             time+=1
-        if fresh==0:
-            return time
-        return -1
-        
-                
+        return time if fresh<=0 else -1
 
 
         
