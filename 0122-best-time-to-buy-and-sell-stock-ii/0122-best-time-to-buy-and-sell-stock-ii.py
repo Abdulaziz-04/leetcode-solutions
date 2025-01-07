3867e3da424b496bce281fe008f827147d62425f
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-01-06 23:04:05 -0700

    Time: 2 ms (50.97%), Space: 13.8 MB (7.05%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,15 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        diff=[]
+        for i in range(1,len(prices)):
+            diff.append(prices[i]-prices[i-1])
         profit=0
-        for i in range(len(prices)-1):
-            if prices[i+1]>prices[i]:
-                profit+=prices[i+1]-prices[i]
+        for d in diff:
+            if d>0:
+                profit+=d
         return profit
-            
-        
         
