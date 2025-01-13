e8e9fac5da8b60a66cdf11c0abd295d0bdb9a4e6
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-01-12 18:32:27 -0700

    Time: 76 ms (22.14%), Space: 16.5 MB (24.72%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,25 +1,20 @@
-class Solution:
-    def numRescueBoats(self, people: List[int], limit: int) -> int:
+class Solution(object):
+    def numRescueBoats(self, people, limit):
         """
-        Let's attempt to pair the most heaviest with the most lightest in boat
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
         """
-        # Sort them
         people.sort()
-        # two pointer to find a pair
         l,r=0,len(people)-1
         boats=0
-        # If both land at same index, we will need one boat as one person remains
         while l<=r:
-            # If pair found move to next persons
-            if people[l]+people[r]<=limit:
-                boats+=1
+            total=people[l]+people[r]
+            if total>limit:
+                r-=1
+            else:
                 l+=1
                 r-=1
-            # If overweight then we need a single boat and we can move to next
-            else:
-                r-=1
-                boats+=1
+            boats+=1
         return boats
-            
-
         
