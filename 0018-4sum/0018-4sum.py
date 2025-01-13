f396348de85e7ce43b61258eea9e378f3ff10de6
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-01-12 18:15:51 -0700

    Time: 518 ms (64.66%), Space: 12.6 MB (14.09%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,23 +1,33 @@
-class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
         nums.sort()
-        result=set()
-        for i,f in enumerate(nums):
+        result=[]
+        for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1,len(nums)):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
                 l,r=j+1,len(nums)-1
                 while l<r:
-                    total=f+nums[j]+nums[l]+nums[r]
-                    if total<target:
-                        l+=1
-                    elif total>target:
-                        r-=1
-                    else:
-                        result.add(tuple([f,nums[j],nums[l],nums[r]]))
+                    total=nums[i]+nums[j]+nums[l]+nums[r]
+                    if total==target:
+                        result.append([nums[i],nums[j],nums[l],nums[r]])
                         l+=1
                         r-=1
-                        while nums[l]==nums[l-1] and l<r:
+                        while l<r and nums[l]==nums[l-1]:
                             l+=1
-                        while nums[r]==nums[r+1] and l<r:
-                            r-=1
-        return list(result)
+                    elif total<target:
+                        l+=1
+                    else:
+                        r-=1
+        return result
+                        
+
+
         
