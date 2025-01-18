62b616964aca2939c56c0a43ec85150eb381f547
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-01-18 13:50:06 -0700

    Time: 4 ms (66.02%), Space: 12.3 MB (19.56%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,37 +1,35 @@
-class Solution:
-    def decrypt(self, code: List[int], k: int) -> List[int]:
-        def calculateSum(i,k,invert=False):
-            c=0
-            total=0
-            if k<0:
-                k=abs(k)
-                invert=not invert
-            if invert:
-                j=i-1
-                while c!=k:
-                    if j==-1:
-                        j=len(code)-1
-                    total+=code[j]
-                    j-=1
-                    c+=1
-            else:
-                j=i+1
-                while c!=k:
-                    if j==len(code):
-                        j=0
-                    total+=code[j]
-                    j+=1
-                    c+=1
-            return total
-        result=[]
-        for i,c in enumerate(code):
-            if c>0:
-                result.append(calculateSum(i,k,False))
-            elif c<0:
-                result.append(calculateSum(i,k,True))
-            else:
-                result.append(0)
+class Solution(object):
+    def decrypt(self, code, k):
+        """
+        :type code: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # Total length of the array
+        N=len(code)
+        # Final result
+        result=[0]*N
+        # Loop through N+k, as we update either l-1 or r+1 pointer
+        l=0
+        curr_sum=0
+        for r in range(N+abs(k)):
+            # Add to sum with circulation
+            curr_sum+=code[r%N]
+            # If we exceed window limit
+            # Remove left value, add mod to avoid out of bounds
+            if r-l+1>abs(k):
+                curr_sum-=code[l%N]
+                l=(l+1)%N
+
+            # If we reach window limit
+            if r-l+1==abs(k):
+                # If <0 then replace l-1 with sum
+                if k<0:
+                    result[(r+1)%N]=curr_sum
+                # if >0 then replace r+1 with sum
+                elif k>0:
+                    result[(l-1)%N]=curr_sum
+            # If k==0, default initialization will handle the output
         return result
 
-            
-
+        
