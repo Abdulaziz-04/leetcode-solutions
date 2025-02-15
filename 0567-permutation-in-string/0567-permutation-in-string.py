25f60dd275921d1a9340d61e45abae3988f59080
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-02-15 15:34:17 -0700

    Time: 16 ms (74.41%), Space: 18.1 MB (18.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,43 +1,35 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1)>len(s2):
             return False
+
         s1_count,s2_count=[0]*26,[0]*26
         matches=0
         for i in range(len(s1)):
             s1_count[ord(s1[i])-ord('a')]+=1
             s2_count[ord(s2[i])-ord('a')]+=1
-        
-        for i in range(26):
-            matches+=1 if s1_count[i]==s2_count[i] else 0
+        for i in range(len(s1_count)):
+            if s1_count[i]==s2_count[i]:
+                matches+=1
 
         l=0
         for r in range(len(s1),len(s2)):
             if matches==26:
                 return True
-
-            r_idx=ord(s2[r])-ord('a')
-            s2_count[r_idx]+=1
-            if s2_count[r_idx]==s1_count[r_idx]:
+            idx=ord(s2[r])-ord('a')
+            s2_count[idx]+=1
+            if s2_count[idx]==s1_count[idx]:
                 matches+=1
-            elif s1_count[r_idx]+1==s2_count[r_idx]:
+            elif s1_count[idx]+1==s2_count[idx]:
                 matches-=1
-
-            l_idx=ord(s2[l])-ord('a')
-            s2_count[l_idx]-=1
-            if s2_count[l_idx]==s1_count[l_idx]:
+            idx=ord(s2[l])-ord('a')
+            s2_count[idx]-=1
+            if s2_count[idx]==s1_count[idx]:
                 matches+=1
-            elif s1_count[l_idx]-1==s2_count[l_idx]:
+            elif s2_count[idx]==s1_count[idx]-1:
                 matches-=1
-            
             l+=1
         return matches==26
 
-            
 
         
