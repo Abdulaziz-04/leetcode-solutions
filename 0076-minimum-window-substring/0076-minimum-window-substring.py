c3f13e74f79a9898a844184afb513bf694190a2e
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-02-16 21:07:07 -0700

    Time: 65 ms (75.81%), Space: 18.3 MB (34.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # Base Case
-        if t=="": return ""
-
-        # Variables
-        window,t_count=defaultdict(int),defaultdict(int)
-        result,mxLen=[-1,-1],float('inf')
-        l=0
-
+        if t=='': return ''
+        window,t_count={},{}
         for ch in t:
-            t_count[ch]+=1
+            t_count[ch]=1+t_count.get(ch,0)
+        l=0
+        resLen,res=float('inf'),[-1,-1]
         have,need=0,len(t_count)
         for r in range(len(s)):
-            window[s[r]]+=1
+            window[s[r]]=1+window.get(s[r],0)
             if s[r] in t_count and window[s[r]]==t_count[s[r]]:
                 have+=1
             while have==need:
-                if r-l+1<mxLen:
-                    mxLen=r-l+1
-                    result=[l,r]
+                if r-l+1<resLen:
+                    resLen=r-l+1
+                    res=[l,r]
                 window[s[l]]-=1
                 if s[l] in t_count and window[s[l]]<t_count[s[l]]:
                     have-=1
                 l+=1
-        l,r=result
-        return s[l:r+1] if mxLen!=float('inf') else ""
+        l,r=res
+        return s[l:r+1] if resLen!=float('inf') else ''
+
+
+
 
         
