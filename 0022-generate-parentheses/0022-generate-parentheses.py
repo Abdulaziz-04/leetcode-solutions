7781596365bd6dcde2a9f6c92bb926ac4c3c413a
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-02-28 00:27:47 -0700

    Time: 0 ms (100%), Space: 18 MB (70.16%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,25 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # 3 conditions for backtrack:
-        """
-            - if open==close==n, then we reached one combo
-            - if open<n then we can add a open and backtrack 
-            - if close<open and close<n, then we can add a close and backtrack
-        """
-        stack=[]
         result=[]
-        def backtrack(open_count,close_count):
-            if open_count==close_count==n:
-                result.append("".join(stack))
+        def backtrack(oc,cc,curr):
+            if oc==n and cc==n:
+                result.append(''.join(curr.copy()))
                 return
-            if open_count<n:    
-                stack.append("(")
-                backtrack(open_count+1,close_count)
-                stack.pop()
-            if close_count<open_count:
-                stack.append(')')
-                backtrack(open_count,close_count+1)
-                stack.pop()
-        backtrack(0,0)        
-        return result        
+            if cc>oc or oc>n or cc>n:
+                return
+            curr.append('(')
+            backtrack(oc+1,cc,curr)
+            curr.pop()
+            curr.append(')')
+            backtrack(oc,cc+1,curr)
+            curr.pop()
+        backtrack(0,0,[])
+        return result
         
