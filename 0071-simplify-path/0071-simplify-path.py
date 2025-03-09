243ea6b16a7bd80372ed0df51a480b268a84743b
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-03-08 17:42:39 -0700

    Time: 3 ms (36.76%), Space: 17.9 MB (47.93%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,21 +1,17 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # Ignore multiple slashes
-        # Ignore the '.'
-        # pop the stack if we encounter '..' i.e. build the string once we cross the slash
-        # As we have to ignore the last slash, let's add it here in the start and deal with the rest
         stack=[]
-        cur=''
-        for c in path+'/':
-            if c=='/':
-                if cur=='..':
+        curr=""
+        for ch in path+'/':
+            if ch=='/':
+                if curr=='..':
                     if stack:
                         stack.pop()
-                elif cur!='' and cur!='.':
-                    stack.append(cur)
-                cur=""
+                elif curr and curr!='.':
+                    stack.append(curr)
+                curr=""
             else:
-                cur+=c
+                curr+=ch
         return '/'+'/'.join(stack)
 
         
