6232e67a7353b38a13ac0208b91e8c5cfe9ad444
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-04-05 18:33:48 -0700

    Time: 2647 ms (6.81%), Space: 18.5 MB (19.63%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,18 +1,17 @@
 from collections import Counter
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        result=set()
+        hmap=set()
         left=set()
         right=Counter(s)
-        for mid in range(len(s)):
-            # Decrement from the right as we encounter
-            right[s[mid]]-=1
-            # If we don't have more characters in right, remove them
-            if right[s[mid]]==0:
-                right.pop(s[mid])
-            for ch in left:
-                if ch in right:
-                    result.add((s[mid],ch))
-            left.add(s[mid])
-        return len(result)
+        for i in range(len(s)):
+            right[s[i]]-=1
+            if right[s[i]]==0:
+                right.pop(s[i])
+            for j in range(26):
+                ch=chr(ord('a')+j)
+                if ch in left and ch in right:
+                    hmap.add((s[i],ch))
+            left.add(s[i])
+        return len(hmap)
         
