c4f8c8d297cf170c7f6a867f6fb92aad4c6b8e4e
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-08-17 23:45:09 -0700

    Time: 3 ms (36.78%), Space: 18.1 MB (27.73%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,28 +1,17 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        # current index
-        idx=0
-        # To modify input array
-        mod_idx=0
-        # Loop through all characters
-        while idx<len(chars):
-            # Keep track of current character
-            curr_char=chars[idx]
-            # Assign init freq
-            freq=1
-            # Check for consecutive characters
-            while idx+1<len(chars) and curr_char==chars[idx+1]:
-                idx+=1
-                freq+=1
-            # Update array and modification index
-            chars[mod_idx]=curr_char
-            mod_idx+=1
-            # Update frequency information
-            if freq>1:
-                for f in str(freq):
-                    chars[mod_idx]=f
-                    mod_idx+=1
-            # Move to next character
-            idx+=1
-        # Return length of modified array
-        return mod_idx
+        r=0
+        i=0
+        while i <len(chars):
+            group=1
+            while i+group<len(chars) and chars[i+group]==chars[i]:
+                group+=1
+            chars[r]=chars[i]
+            r+=1
+            if group>1:
+                str_group=str(group)
+                chars[r:r+len(str_group)]=list(str_group)
+                r+=len(str_group)
+            i+=group
+        return r
+        
