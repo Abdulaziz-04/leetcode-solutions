0370057dbf466ba49f9b6234bec15d5e7cdc868a
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-12-21 00:44:54 -0500

    Time: 23 ms (19.13%), Space: 22 MB (35.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def calculate_frequency(word):
-            word_freq=[0 for i in range(26)]
-            for letter in word:
-                word_freq[ord(letter)-ord('a')]+=1
-            return tuple(word_freq)
-        freq_map={}
-        for word in strs:
-            word_freq=calculate_frequency(word)
-            if word_freq in freq_map:
-                freq_map[word_freq].append(word)
-            else:
-                freq_map[word_freq]=[word]
-        return freq_map.values()
-
-        
+        def get_tuple(s):
+            chars=[0]*26
+            for ch in s:
+                chars[ord(ch)-ord('a')]+=1
+            return tuple(chars)
+        anagrams=defaultdict(list)
+        for s in strs:
+            anagrams[get_tuple(s)].append(s)
+        result=[]
+        for k,v in anagrams.items():
+            result.append(v)
+        return result
+            
+            
+         
