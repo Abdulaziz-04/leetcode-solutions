83411a7db9bcf633ede98135eac07e26d4b48169
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2025-12-21 00:01:56 -0500

    Time: 19 ms (24.66%), Space: 19.3 MB (99.88%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,39 +4,30 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def merge_list(self,l1,l2):
-        dummy=ListNode(-1,None)
-        curr=dummy
-        while l1 and l2:
-            if l1.val<l2.val:
-                curr.next=l1
-                l1=l1.next
-            else:
-                curr.next=l2
-                l2=l2.next
-            curr=curr.next
-        if l1:
-            curr.next=l1
-        if l2:
-            curr.next=l2
-        return dummy.next
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists)==0:
+        if not lists:
             return None
-        
+        def merge_list(l1,l2):
+            dummy=ListNode()
+            curr=dummy
+            while l1 and l2:
+                if l1.val<l2.val:
+                    curr.next=l1
+                    l1=l1.next
+                else:
+                    curr.next=l2
+                    l2=l2.next
+                curr=curr.next
+            if l1:
+                curr.next=l1
+            if l2:
+                curr.next=l2
+            return dummy.next
         while len(lists)>1:
-            curr_lists=[]
+            new_lists=[]
             for i in range(0,len(lists),2):
-                l1=lists[i]
-                l2=lists[i+1] if i+1<len(lists) else None
-                curr_lists.append(self.merge_list(l1,l2))
-            lists=curr_lists
+                l1,l2=lists[i],lists[i+1] if i+1<len(lists) else None
+                new_lists.append(merge_list(l1,l2))
+            lists=new_lists
         return lists[0]
-
-
-            
-
-
-
-
         
