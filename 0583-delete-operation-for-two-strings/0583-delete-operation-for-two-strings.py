ef312442b52db8c48376a2364797ed93553d6faf
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2026-03-16 22:17:34 -0400

    Time: 162 ms (11.54%), Space: 54.8 MB (12.14%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n,m=len(word1),len(word2)
+        memo={}
+        def dfs(i,j):
+            if i<0 or j<0:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if word1[i]==word2[j]:
+                memo[(i,j)]=1+dfs(i-1,j-1)
+            else:
+                memo[(i,j)]=max(dfs(i-1,j),dfs(i,j-1))
+            return memo[(i,j)]
+            
+        return n+m-2*dfs(n-1,m-1)
